2a1a4621167713e2a7ee48a2fc7959e4ec54dcf9
pb3oh <philnb331@gmail.com>
2026-04-28 17:58:11 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BenefitsAudit from "./BenefitsAudit.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BenefitsAudit />
-  </React.StrictMode>
-);
